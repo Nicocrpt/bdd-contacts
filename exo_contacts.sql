9898559a9c048a2d3c5288e7ab76fe6c6c0d7f8a
nicolas carpita <carpita.nicolas@gmail.com>
2023-11-23 09:23:52 +0100

    Creation table pays

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE IF NOT EXISTS exo_contacts CHARACTER SET = "utf8" COLLATE = "utf8_general_ci" ;
 
-USE exo_contacts
+USE exo_contacts ;
+
+CREATE TABLE IF NOT EXISTS pays (
+    iso_3 VARCHAR(3) PRIMARY KEY NOT NULL,
+    nom VARCHAR(70) NOT NULL,
+    iso_2 VARCHAR(2) NOT NULL,
+    nationalité VARCHAR(50) NOT NULL
+);
