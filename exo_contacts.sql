7b83bac544548c3327ed301237c4fa9ef2017679
nicolas carpita <carpita.nicolas@gmail.com>
2023-11-23 09:39:39 +0100

    Ajout contenu table pays + affichage

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -8,3 +8,18 @@ CREATE TABLE IF NOT EXISTS pays (
     iso_2 VARCHAR(2) NOT NULL,
     nationalité VARCHAR(50) NOT NULL
 );
+
+INSERT INTO pays (iso_3, nom, iso_2, nationalité) VALUES
+("FRA", "France", "FR", "Francais"),
+("ISL", "Islande", "IS", "Islandais"),
+("JPN", "Japon", "JP", "Japonais"),
+("AUS", "Australie", "AU", "Australien"),
+("CHN", "Chine", "CN", "Chinois"),
+("NPL", "Nepal", "NP", "Nepalais"),
+("DEU", "Allemagne", "DE", "Allemand"),
+("BEL", "Belgique", "BE", "Belge"),
+("GBR", "Royaume-Uni", "GB", "Anglais"),
+("NGA", "Nigeria", "NG", "Nigerien");
+
+SELECT * FROM pays ;
+
